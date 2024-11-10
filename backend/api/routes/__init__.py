f4c686aa8d4524ce6a039ef9efa8ffa27142d373
bryanlzl <zhenlun.bryan@gmail.com>
2024-11-10 10:27:56 +0800

    fix: render port fix

--- backend/api/routes/__init__.py
+++ backend/api/routes/__init__.py
@@ -11,7 +11,7 @@ from .predict import router as predict_router
 app = FastAPI()
 
 host = os.getenv("BACKEND_IP", "0.0.0.0")
-port = int(os.getenv("BACKEND_PORT", 8000))
+port = int(os.getenv("PORT", os.getenv("BACKEND_PORT", 8000)))
 
 if __name__ == "__main__":
     uvicorn.run(app, host=host, port=port)
