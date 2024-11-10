2e97fdcbd96cc51412b26e109d716b8ff403419c
bryanlzl <zhenlun.bryan@gmail.com>
2024-11-10 10:11:08 +0800

    change: backend server default ip as 0.0.0.0 with port 8000

--- backend/api/routes/__init__.py
+++ backend/api/routes/__init__.py
@@ -1,14 +1,17 @@
 import os
 
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+load_dotenv()
 
 from .predict import router as predict_router
 
 app = FastAPI()
 
-host = os.getenv("HOST", "0.0.0.0")  
-port = int(os.getenv("PORT", 8000))
+host = os.getenv("BACKEND_IP", "0.0.0.0")
+port = int(os.getenv("BACKEND_PORT", 8000))
 
 if __name__ == "__main__":
     uvicorn.run(app, host=host, port=port)
