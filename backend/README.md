979cd9852976edf5688a4c0019ec17bc6c13bd45
bryanlzl <zhenlun.bryan@gmail.com>
2024-11-14 11:18:01 +0800

    change: readme, envs and gitignore confgs

--- backend/README.md
+++ backend/README.md
@@ -2,4 +2,4 @@
 
 1. cd to backend root dir
 2. run `pip install -r requirements.txt`
-3. run `uvicorn api.main:app --host 0.0.0.0 --port 8000 --reload`
+3. run `uvicorn api.main:app --host --port 8000 --reload`
--- backend/api/main.py
+++ backend/api/main.py
@@ -8,4 +8,4 @@ host = os.getenv("BACKEND_IP", '0.0.0.0')
 port = int(os.getenv("BACKEND_PORT", 8000))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=port)
+    uvicorn.run(app, host=host, port=port)
